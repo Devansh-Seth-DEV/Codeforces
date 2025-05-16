b5099415cfa0f58af3a851096c98d9aefcf464b6
Devansh Seth <dev.devanshseth.engg0072@gmail.com>
2025-05-17 02:14:37 +0530

    Done 1st 1600 rating question

--- D1600/2082a_floor_or_ceil.cpp
+++ D1600/2082a_floor_or_ceil.cpp
@@ -12,6 +12,22 @@ using namespace std;
 
 typedef long long ll;
 
+int F(int x, int n) {
+	while(n--) {
+		if (!x) return x;
+		x >>= 1;
+	}
+	return x;
+}
+
+int C(int x, int n) {
+	while(n--) {
+		if (x<=1) return x;
+		x = (x+1)>>1;
+	}
+	return x;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); 
@@ -20,7 +36,9 @@ int main() {
 	cin >> t;
 
 	while(t--) {
-
+		int x, n, m;
+		cin >> x >> n >> m;
+		cout << F(C(x, m), n) << " " << C(F(x, n), m) << "\n";
 	}
     
     return 0;
