456d07615c913144dbd385d2c00a629cc692ac8f
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2025-05-17 06:32:49 +0530

    Create 2057a_mex_table.cpp

---
+++ D800/2057a_mex_table.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,s,e) for(int i=s; i<e; i++)
+#define REPR(i,s,e) for (int i=s; i>e; i--)
+
+#define SWP(a,l,r)	int temp=a[r]; \
+					a[r]=a[l]; \
+					a[l]=temp;
+
+#define NEW(n,type) (type*) malloc(sizeof(type)*(n))
+
+typedef long long ll;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0); 
+    
+	int t;
+	cin >> t;
+
+	while(t--) {
+    int n, m;
+    cin >> n >> m;
+    cout << max(n, m)+1 << "\n";
+	}
+    
+  return 0;
+}
