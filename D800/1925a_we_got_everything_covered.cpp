e3a3216a3a7564fa255eed1b1dfb055818a74c2e
Devansh Seth <dev.devanshseth.engg0072@gmail.com>
2025-05-24 05:01:27 +0530

    Done 1 more questions

---
+++ D800/1925a_we_got_everything_covered.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,s,e) for(int i=s; i<e; i++)
+#define REPR(i,s,e) for (int i=s; i>e; i--)
+
+#define SWP(a,l,r)	int temp=a[r]; \
+					a[r]=a[l]; \
+					a[l]=temp;
+
+#define NEW(n,type) (type*) malloc(sizeof(type)*(n))
+#define RI(i) int i; cin >> i
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); 
+    
+	RI(t);
+
+	while(t--) {
+		RI(n); RI(k);
+		REP(i,0,n) {
+			REP(j,0,k) {
+				char c='a'+j;
+				cout << c;
+			}
+		}
+		cout << "\n";
+	}
+    
+    return 0;
+}
