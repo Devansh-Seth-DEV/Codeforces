914e8080cbd5b192d05692506a6d050239d2a067
Devansh Seth <dev.devanshseth.engg0072@gmail.com>
2025-05-30 02:32:10 +0530

    Updated the boilder macros

--- contests/div3/1027/B.cpp
+++ contests/div3/1027/B.cpp
@@ -4,11 +4,6 @@ using namespace std;
 #define FOR(i,s,e) for(int i=s; i<e; i++)
 #define FOR_REV(i,s,e) for (int i=s; i>e; i--)
 
-#define SWP(A,i,j) \
-	int temp=A[j]; \
-	A[j]=A[i]; \
-	A[i]=temp;
-
 #define INIT_NEW_NUM_ARGS_IMPL(_1, _2, _3, _4, _5, N, ...) N
 #define INIT_NEW_NUM_ARGS(...) INIT_NEW_NUM_ARGS_IMPL(__VA_ARGS__, 5, 4, 3, 2)
 
@@ -20,11 +15,11 @@ using namespace std;
         src[i] = temp;\
         temp += inc;\
     }
-#define INIT_NEW_ARGS_5(src,n,v,oprnd,op) \
+#define INIT_NEW_ARGS_5(src,n,v,opr,op) \
 	int temp=v;\
 	FOR(i,0,n) {\
 		src[i] = temp;\
-		temp = temp op oprnd;\
+		temp = temp op opr;\
 	}
 
 #define INIT_NEW_CHOOSER(...) INIT_NEW_NUM_ARGS(__VA_ARGS__)
@@ -32,11 +27,18 @@ using namespace std;
 #define INIT_NEW_EXPAND_HELPER(count, ...) INIT_NEW_ARGS_##count(__VA_ARGS__)
 #define INIT_NEW_EXPAND(count, ...) INIT_NEW_EXPAND_HELPER(count, __VA_ARGS__)
 
+#define SWP(A,i,j) \
+	int temp=A[j]; \
+	a[j]=a[i]; \
+	a[i]=temp;
 
 #define NEW(n,type) (type*) malloc(sizeof(type)*(n))
 #define INIT_NEW(...) INIT_NEW_EXPAND(INIT_NEW_CHOOSER(__VA_ARGS__), __VA_ARGS__)
 
 #define RI(i) int i; cin >> i
+#define RC(c) char c; cin >> c
+
+typedef long long ll;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); 
@@ -44,9 +46,21 @@ int main() {
 	RI(t);
 
 	while(t--) {
-		RI(n);
+		RI(n); RI(k);
 
+		cin.ignore();
+		int zc=0, oc=0;
+		FOR(i,0,n) {
+			RC(x);
+			if (x=='0') zc++;
+		}
+		oc = n-zc;
+		int min = max(zc, oc) - (n>>1);
+		int max = (zc>>1) + (oc>>1);
+		if (k>=min && !((k-min)&1) && k<=max) cout << "YES\n";
+		else cout << "NO\n";
 	}
     
     return 0;
 }
+
